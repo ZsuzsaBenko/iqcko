2f78a3bb18685f2d64a05eb5a59645d370f5562a
ZsuzsaBenko <benkozsuzsa11@gmail.com>
2019-09-22 18:37:34 +0200

    Add Firebase configuration to production env, too

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAxPmUIarzur4UtDYrR-9Qq_jbQgUvH7Y4',
+    authDomain: 'puzzles-a6dd7.firebaseapp.com',
+    databaseURL: 'https://puzzles-a6dd7.firebaseio.com',
+    projectId: 'puzzles-a6dd7',
+    storageBucket: 'puzzles-a6dd7.appspot.com',
+    messagingSenderId: '1087905123552',
+    appId: '1:1087905123552:web:16d664df5fddeff8251485'
+  }
 };
