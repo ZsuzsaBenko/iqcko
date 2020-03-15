8c45c70e6fd098c63672de5a060bdc91b9693319
ZsuzsaBenko <benkozsuzsa11@gmail.com>
2020-03-15 21:28:15 +0100

    Use Angular 9

--- src/app/services/auth-interceptor.service.ts
+++ src/app/services/auth-interceptor.service.ts
@@ -1,6 +1,8 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     if (!req.url.endsWith('registration') && !req.url.endsWith('login')) {
